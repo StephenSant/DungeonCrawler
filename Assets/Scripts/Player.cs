11368f4b7eda40ce431389419c835c5f519e4c0a
Stephen <42423953+StephenSant@users.noreply.github.com>
2019-05-03 16:05:08 +1000

    BAM

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    public CharacterController controller;
+
+    private float inputH, inputV;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    float ToDiagonal(float x, float y)
+    {
+        return x + y;
+    }
+
+    void Update()
+    {
+        Vector3 prevVector = transform.position;
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+        controller.SimpleMove(new Vector3 (ToDiagonal(-inputV,inputH),0, ToDiagonal(inputV, inputH)) * speed * Time.deltaTime);
+        Debug.Log(prevVector-transform.position);
+    }
+}
